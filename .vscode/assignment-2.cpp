d7f94f5a30420d6429685943f45c86398b007602
stav1019 <stavsendrovitz@gmail.com>
2022-03-22 20:31:46 -0700

    commit?

---
+++ .vscode/assignment-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    string a;
+    string b;
+    string c;
+    string a = "We have made a long journey of 4276 miles.";
+    string b = "Our checking account balance is -20.";
+    string; c = "About 189000 days ago Columbus stood on this spot.";
+
+    std::string << string a + string b + string c << std::endl;
+}
