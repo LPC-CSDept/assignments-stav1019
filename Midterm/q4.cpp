96f50730370d73a6f13f0c871444e95a90655fd3
stav1019 <stavsendrovitz@gmail.com>
2022-04-20 00:46:59 -0700

    commit4-question4

--- Midterm/q4.cpp
+++ Midterm/q4.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-
+int n1 = 10;
+int n2 = 15;
 int x;
 int y;
 int z;
@@ -20,6 +21,21 @@ int getRdnum(){
     
     return x;
    
+}
+int getRdnum(int n2){
+     
+     y = (rand() % (n2)) + 1;
+     
+     return y;
+}
+
+int getRdnum(int n1, int n2)
+{
+    
+    z = (rand() % (n2 - n1)) + (n1 + 1);
+   
+   return z;
+   // return (rand() % n2) + n1;
 }
 int main()
 {
