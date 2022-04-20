c8904417e197b39c28d7db0120871e3aad0b3a5b
stav1019 <stavsendrovitz@gmail.com>
2022-04-20 00:56:23 -0700

    commit2-question5

---
+++ Midterm/q5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int num1;
+int num2;
+int divResult;
+
+int getInput()
+{
+    
+   
+    cout << "Please enter 2 different numbers: \n" ;
+
+      cin >> num1 >> num2;
+      
+    
+
+    return num1,num2;
+}
+
+int isSame(){
+    if (num1 == num2){
+    cout << "program closing";
+    exit(0);
+}
+return 0;
+}
+
+int intDivision(){
+    if (num1 > num2){
+        divResult = num1/num2;
+    }
+    else if (num2 > num1){
+        divResult = num2/num1;
+    }
+    return 0;
+}
