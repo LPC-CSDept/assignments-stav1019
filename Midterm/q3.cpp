8ac3a2b63fd164cbb677a6345a1ab0063af1c7aa
stav1019 <stavsendrovitz@gmail.com>
2022-04-20 00:08:16 -0700

    commit1-quesiton3

---
+++ Midterm/q3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <time.h>
+
+
+using namespace std;
+
+
+int N = 10;
+int num = 0;
+int x;
+ int getRdnum(void){
+     
+      
+     x = (rand() % 50) + 1;
+      
+      return x;
+  }
+ 
