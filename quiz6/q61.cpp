6feb4a534213f683295cf13bbde82847bcc06ac8
stav1019 <stavsendrovitz@gmail.com>
2022-05-14 07:47:26 -0700

    commit2 q61.cpp

--- quiz6/q61.cpp
+++ quiz6/q61.cpp
@@ -6,19 +6,20 @@ using namespace std;
 
 void swapTwoValues(int n1, int n2){
   
-     
+      cout << "initial values: \nn1: " << n1 << "\nn2: " << n2 << "\n";    
+
     swap(n1, n2);
     
-      
+      cout << "swapped values: \nn1: " << n1 << "\nn2: " << n2 << "\n(int version of overloaded function)";  
 }
 
 void swapTwoValues(float n1, float n2){
 
-      
+        cout << "initial values: \nn1: " << n1 << "\nn2: " << n2 << "\n";  
       
        swap(n1, n2);
        
-        
+       cout << "swapped values: \nn1: " << n1 << "\nn2: " << n2 << "\n(float version of overloaded function)";  
 }
 
 
@@ -32,6 +33,11 @@ int main()
     
     swapTwoValues(intN1, intN2);
     
-   
+    cout << "\n now, please enter 2 floats(n1, n2):\n ";
+    cin >> fN1 >> fN2;
+    
+    swapTwoValues(fN1, fN2);
+    
+    
     return 0;
 }
