9f080d641cee1c70503342defdd93dc980c8bf66
stav1019 <stavsendrovitz@gmail.com>
2022-05-15 04:52:00 -0700

    2-10

---
+++ c2assignments/2-10.cpp
@@ -0,0 +1,16 @@
+//program 2-10, assignment 2-1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int journeyDistance = 4276;
+    int balance = -20;
+    int daysAgo = 189000;
+    
+    cout<<"We have made a long journey of " << journeyDistance << " miles.\nOur checking account balance is " << balance << ".\nAbout " << daysAgo << " days ago Colombus stood on this spot.";
+
+    return 0;
+}
