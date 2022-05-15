a182bbab91c1faf7a86d9dfa85fbf7590202465f
stav1019 <stavsendrovitz@gmail.com>
2022-05-15 03:29:49 -0700

    remake quiz7 folder

---
+++ Quiz7/realq4.cpp
@@ -0,0 +1,61 @@
+#include	<iostream>
+#include	<cstdlib>
+#include	<ctime>
+#include	<vector>
+#include	<algorithm>
+#include	<iterator>
+using namespace std;
+
+void makevector(vector<int> &, int);
+void printvector(vector<int>);
+void deleteone(vector<int> &);
+int main()
+{
+	vector<int> number;
+	int 	size = 20;
+	srand(time(0));
+	makevector(number, size);
+	printvector(number);
+	deleteone(number);
+}
+void makevector(vector<int> &vec, int size)
+{
+	for(int i=0; i<size; i++)
+    	    vec.push_back(rand() % 10);
+}
+void printvector(vector<int> vec)
+{
+	for(int v : vec)
+    	     cout << v << "\t";
+	cout << endl;
+}
+void deleteone(vector<int> &vec)
+{
+      int usernum, deletecnt=0;
+      cin >> usernum;
+      
+      
+      
+      
+vector<int>::iterator it;
+  
+it = std::find(vec.begin(), vec.end(), usernum);
+//int deletecnt = 0;
+  
+while(it != vec.end())
+{
+vec.erase(it);
+  deletecnt++;
+
+it = std::find(vec.begin(), vec.end(), usernum);
+}
+  
+
+
+      // Your Code 
+
+      cout << usernum << "is deleted " << deletecnt << " times \n";
+	printvector(vec);
+	
+}
+
