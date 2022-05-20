63a2c31a5a56252cab09a6437e5051a612dcaa43
stav1019 <stavsendrovitz@gmail.com>
2022-05-19 19:17:40 -0700

    q91

---
+++ Quiz9/Q91.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+struct Employee{
+    int id;
+    string name;
+    int salary;
+    string depName;
+    string date;
+
+};
+
+int main()
+{
+   
+    
+}
