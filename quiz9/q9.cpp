0da7e7d7397953225b1867d9e33e33cb924ec5c1
stav1019 <stavsendrovitz@gmail.com>
2022-05-20 09:13:38 -0700

    q9

--- quiz9/q9.cpp
+++ quiz9/q9.cpp
@@ -4,17 +4,28 @@
 using namespace std;
 
 
-
+//struct Date{
+ //   string date;
+    //int date2;
+   // int date3;
+//};
 
 struct Employees{
     int id;
-    string name;
+    string name1;
+    string name2;
     double salary;
     string depName;
     string date;
+    int date2;
+    int date3;
 };
 
 
+
+int makeEmployeeStructArray(Employees e[]);
+void printEmployee(Employees e);
+void findEmployee(Employees e[], int maxemployeeNum);
 
 
 
@@ -25,24 +36,25 @@ int main()
   
    
    int maxemployeeNum;
-   maxemployeeNum = makeEmployeeStructArray(e);
+   maxemployeeNum = makeEmployeeStructArray(e, 50);
    
-  
 
 
 
-for (int i = 0; i < maxemployeeNum; i++){
-        printEmployee(e[i]);
-    }
 
-    findEmployee(e);
+//for (int i = 0; i < maxemployeeNum; i++){
+        //printEmployee(e[i]);
+        //cout << "\nNUM: " << makeEmployeeStructArray(e) << "\n\n";
+    //}
+
+    findEmployee(e , maxemployeeNum);
     
    
   
 }
 
 
-int makeEmployeeStructArray(Employees e[]){
+int makeEmployeeStructArray(Employees e[], int maxemployeeNum){
     
 
     ifstream ifs;
@@ -52,27 +64,30 @@ int makeEmployeeStructArray(Employees e[]){
     if (ifs){
         idx = 0;
         for (int i = 0; i < maxemployeeNum; i++){
-            ifs >> e[idx].id >> e[idx].name1 >> e[idx].salary >> e[idx].depName >> e[idx].date;
+            ifs >> e[idx].id >> e[idx].name1 >> e[idx].name2 >> e[idx].salary >> e[idx].depName >> e[idx].date >> e[idx].date2 >> e[idx].date3;
              idx += 1;
         }
-        
+        //ifs >> e[idx].id >> e[idx].name1 >> e[idx].name2 >> e[idx].salary >> e[idx].depName >> e[idx].date >> e[idx].date2 >> e[idx].date3;
+           //  idx += 1;
         }
     
         return idx;
     }
    void findEmployee(Employees e[], int maxemployeeNum){
+     //  int isPrinted = 0;
+       cout << "qualified employees:\n\n";
        for (int i=0;i < maxemployeeNum;i++){
            if (e[i].salary > 100000 && e[i].depName == "Computer"){
              
                  printEmployee(e[i]);
-                 
+                //  isPrinted++ ;
            }
      }
    }
     
     void printEmployee(Employees e){
-    cout << "ID: " <<  e.id << " Name: " << e.name1 << " salary: " << e.salary << " department: " << e.depName << " date: " << e.date;
-  
+    cout << "ID: " <<  e.id << " Name: " << e.name1 << " " << e.name2 << " salary: " << e.salary << " department: " << e.depName << " date: " << e.date << " " << e.date2 << " " << e.date3 << "\n";
+   
     }
     
 
