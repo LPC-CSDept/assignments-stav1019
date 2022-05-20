6490532c03a4febe608b2ada7931d7d5d881472f
stav1019 <stavsendrovitz@gmail.com>
2022-05-20 09:27:56 -0700

    q9

--- quiz9/q9.cpp
+++ quiz9/q9.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 
-//struct Date{
- //   string date;
-    //int date2;
-   // int date3;
-//};
+
 
 struct Employees{
     int id;
@@ -23,7 +19,7 @@ struct Employees{
 
 
 
-int makeEmployeeStructArray(Employees e[]);
+int makeEmployeeStructArray(Employees e[], int maxemployeeNum);
 void printEmployee(Employees e);
 void findEmployee(Employees e[], int maxemployeeNum);
 
@@ -37,15 +33,6 @@ int main()
    
    int maxemployeeNum;
    maxemployeeNum = makeEmployeeStructArray(e, 50);
-   
-
-
-
-
-//for (int i = 0; i < maxemployeeNum; i++){
-        //printEmployee(e[i]);
-        //cout << "\nNUM: " << makeEmployeeStructArray(e) << "\n\n";
-    //}
 
     findEmployee(e , maxemployeeNum);
     
@@ -67,27 +54,25 @@ int makeEmployeeStructArray(Employees e[], int maxemployeeNum){
             ifs >> e[idx].id >> e[idx].name1 >> e[idx].name2 >> e[idx].salary >> e[idx].depName >> e[idx].date >> e[idx].date2 >> e[idx].date3;
              idx += 1;
         }
-        //ifs >> e[idx].id >> e[idx].name1 >> e[idx].name2 >> e[idx].salary >> e[idx].depName >> e[idx].date >> e[idx].date2 >> e[idx].date3;
-           //  idx += 1;
         }
     
         return idx;
     }
    void findEmployee(Employees e[], int maxemployeeNum){
-     //  int isPrinted = 0;
+       
        cout << "qualified employees:\n\n";
        for (int i=0;i < maxemployeeNum;i++){
            if (e[i].salary > 100000 && e[i].depName == "Computer"){
              
                  printEmployee(e[i]);
-                //  isPrinted++ ;
+                  
            }
      }
    }
     
     void printEmployee(Employees e){
-    cout << "ID: " <<  e.id << " Name: " << e.name1 << " " << e.name2 << " salary: " << e.salary << " department: " << e.depName << " date: " << e.date << " " << e.date2 << " " << e.date3 << "\n";
-   
+    cout << "ID: " <<  e.id << " Name: " << e.name1 << " " << e.name2 << " Salary: $" << e.salary << " Department: " << e.depName << " Date: " << e.date << " " << e.date2 << " " << e.date3 << "\n";
+ 
     }
     
-
+    
